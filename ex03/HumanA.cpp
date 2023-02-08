985d3933c530809ab15ddafa7631daf32789f77a
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-02-08 18:07:42 +0900

    ex03

---
+++ ex03/HumanA.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "HumanA.hpp"
+
+HumanA::HumanA(const std::string &name, const Weapon &weapon) : name_(name), weapon_(weapon) {}
+HumanA::~HumanA() {}
+
+void HumanA::attack()
+{
+	std::cout << name_ << " attacks with their" << weapon_.getType() << std::endl;
+}
