925839da48fa5aabef5eefdba114af06ed0489b9
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-02-09 20:59:19 +0900

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
 #include "SedManager.hpp"
 
 int main(int argc, char **argv)
 {
-	
+	SedManager sed_manager;
+	std::string filename;
+
+	if (argc != 4)
+	{
+		std::cout << "Error: Wrong Arguments [filename] [s1] [s2]." << std::endl;
+		return (1);
+	}
+	filename = argv[1];
+	sed_manager.check_valid_file(filename, filename + ".replace");
+	sed_manager.replace_str(argv[2], argv[3]);
+	sed_manager.close_file();
+
+	return (0);
 }
