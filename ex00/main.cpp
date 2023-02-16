270f0d83a7a5455b8aefc36e7daf122764fb3beb
Jeongki Park <jeongkpa@cx1r4s5.42seoul.kr>
2023-02-16 20:07:17 +0900

    init4

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,20 +1,44 @@
-#include "Fixed.hpp"
+#include <iostream>
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongDog.hpp"
+#include "WrongCat.hpp"
 
-/*
-	a 객체 생성
-	b 객체는 a를 복사 생성
-	c 객체는 대입 연산자 오버로딩 b를 복사
-*/
-int main(void)
-{
-	Fixed	a;
-	Fixed	b(a);
-	Fixed	c;
+int main(void) {
+  std::cout << "===========Correct Way===========" << std::endl;
+  const Animal *meta = new Animal();
+  const Animal *j = new Dog();
+  const Animal *i = new Cat();
 
-	c = b;
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	return 0;
+  i->makeSound();//will output the cat sound!
+  j->makeSound();
+  meta->makeSound();
+
+  delete meta;
+  delete j;
+  delete i;
+
+  std::cout << "===========Wrong Way===========" << std::endl;
+
+  const WrongAnimal *metaw = new WrongAnimal();
+  const WrongAnimal *jw = new WrongDog();
+  const WrongAnimal *iw = new WrongCat();
+
+  std::cout << jw->getType() << " " << std::endl;
+  std::cout << iw->getType() << " " << std::endl;
+
+  iw->makeSound();//will output the cat sound!
+  jw->makeSound();
+  metaw->makeSound();
+
+  delete metaw;
+  delete jw;
+  delete iw;
+
+  return 0;
 }
