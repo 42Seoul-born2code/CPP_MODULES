03cf1d587ea2daefb5ae37a5ef1bcb97021bd992
Jeongki Park <jeongkpa@cx1r4s5.42seoul.kr>
2023-02-16 20:09:22 +0900

    Animal.cpp

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -1,25 +1,35 @@
 #include "Animal.hpp"
 #include <iostream>
 
-Animal::Animal(void) : type_("animal") {
+Animal::Animal(void) : type_("animal") 
+{
   std::cout << "Default constructor of Animal called" << std::endl;
 }
-Animal::Animal(const Animal &copy) : type_(copy.getType()) {
+
+Animal::Animal(const Animal &copy)
+{
+  type_ = copy.getType();
   std::cout << "Copy constructor of Animal called" << std::endl;
 }
-Animal::~Animal() {
+
+Animal::~Animal() 
+{
   std::cout << "Destructor of Animal called" << std::endl;
 }
 
-Animal &Animal::operator=(const Animal &src) {
+Animal &Animal::operator=(const Animal &src) 
+{
   this->type_ = src.getType();
   std::cout << "Assignment Operator of Animal called" << std::endl;
   return (*this);
 }
 
-void Animal::makeSound(void) const {
+void Animal::makeSound(void) const 
+{
   std::cout << "Don't have sound yet..." << std::endl;
 }
-const std::string &Animal::getType() const {
+
+const std::string &Animal::getType() const 
+{
   return type_;
 }
