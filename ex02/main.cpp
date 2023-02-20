b1035bceef451a6e9854dbd8dd5717aceaecf17c
Jeongki Park <jeongkpa@cx1r1s1.42seoul.kr>
2023-02-20 09:58:06 +0900

    주석 추가

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "Fixed.hpp"
 
+/*
+	1/256 = 0.0390625
+
+	0.0390625 * 2 = 0.0078125
+
+	float계산에서 오차가 생겨서 10.1016 처럼 뒤에 소수점 자리 오차가 발생할 수 있음
+*/
 int main( void ) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
