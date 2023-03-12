a2b99e042c8916640f1532fcb2a22300c947ff04
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-03-12 17:35:10 +0900

    주석

--- ex00/Convertor.cpp
+++ ex00/Convertor.cpp
@@ -29,11 +29,32 @@ bool Convertor::isNan(double num) const {
     return true;
   return false;
 }
+
+/*
+C++에서, 양의 무한대는 1.0 / 0.0 으로 표현하고, 음의 무한대는 -1.0 / 0.0으로 표현합니다. 이것은 부동소수점 연산에서 특수한 값으로, 특별한 의미를 가지며 무한대로 표현됩니다.
+*/
+
 bool Convertor::isInf(double num) const {
   if (num == (1.0 / 0.0) || num == (-1.0 / 0.0))
     return true;
   return false;
 }
+
+/*
+strtod() 함수가 이 문자열을 double형으로 변환하고, 그 결과를 반환합니다. 
+반환된 값을 int형으로 변환하여 num에 저장하기 위해 static_cast<int>를 사용하였습니다. 
+이렇게 함으로써 double형에서 int형으로의 형변환을 수행하면서, 소수점 이하의 값은 버리고, 정수 부분만을 유지하게 됩니다.
+
+static_cast를 사용하는 이유는, C++에서는 데이터 타입 간의 명시적인 형변환을 할 때, static_cast를 이용하여 명시적으로 형변환을 하도록 권장하고 있기 때문입니다.
+
+데이터 타입 간의 명시적인 형변환을 하는 이유는, 다른 데이터 타입으로 변환하여 저장하거나 계산할 때, 데이터 손실을 최소화하기 위해서입니다. 
+예를 들어 double형 데이터를 int형으로 변환할 때, 소수점 이하 값은 버려지므로 데이터 손실이 발생합니다. 
+이때 static_cast를 사용하여 명시적으로 형변환을 하면, 컴파일러가 형변환을 할 때 발생할 수 있는 오류를 검출해 줍니다. 
+따라서 static_cast를 사용하여 명시적으로 형변환을 하는 것이 안전하고 권장되는 방법
+*/
+
+
+// 포인터가 들어가야 해서 value.c_str()
 void Convertor::convertChar() const {
   int num;
 
@@ -53,8 +74,10 @@ void Convertor::convertChar() const {
     std::cout << "impossible" << std::endl;
   }
 }
+
 void Convertor::convertInt() const {
   int num = std::atoi(value_.c_str());
+  std::cout << num << std::endl;
 
   std::cout << "int: ";
   if (num == 0 && value_[0] != '0') {
